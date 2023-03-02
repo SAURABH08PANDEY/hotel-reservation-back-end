bcca443de9c95390f5dcdf04af8daf43f389de0a
SAURABH08PANDEY <80577220+SAURABH08PANDEY@users.noreply.github.com>
2023-03-02 22:37:25 +0530

    next

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const express = require('express');
-require('dotenv').config();
-
+const express = require("express");
 const app = express();
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const cors = require('cors');
 const connectDB = require("./db/connect");
 app.use(cors());
 app.use(express.json());
-const bookingRouter = require('./routes/bookingRoutes');
+const bookingRouter = require("./routes/bookingRoutes");
 
-app.get('/', (req, res) => {
-    res.send( 'Home Page')
-})
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
-app.use('/api/v1',bookingRouter )
-
+app.use("/api/v1", bookingRouter);
 
 const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URL);
-      app.listen(port);
-      console.log(`Server started on port ${port}`);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port);
+    console.log(`Server started on port ${port}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
 start();
-  
-module.exports = app;
