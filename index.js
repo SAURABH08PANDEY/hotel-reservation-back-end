570339c25963ed6a5b5c72d91ecfeef2ab0fc370
SAURABH08PANDEY <80577220+SAURABH08PANDEY@users.noreply.github.com>
2023-03-02 21:56:21 +0530

    deleted some packages

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-const morgan = require("morgan");
+
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const connectDB = require("./db/connect");
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
 const bookingRouter = require('./routes/bookingRoutes');
 
 app.get('/', (req, res) => {
